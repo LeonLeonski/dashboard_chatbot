df4bf430f3c11e94e63daf3cd24e82fea06fc276
leonleonski-byte <leon.wink@stud.bbbaden.ch>
2020-12-19 03:18:35 +0100

    Login with firebase

--- src/App.js
+++ src/App.js
@@ -1,49 +1,229 @@
-import logo from './logo.svg';
 import './App.css';
-
 import React from "react";
 import { GetOrders } from "./orders";
-
+import firebase from "firebase";
+import {
+    FirebaseAuthProvider,
+    IfFirebaseAuthed,
+    IfFirebaseUnAuthed
+} from "@react-firebase/auth";
+import {config} from "./test-credentials.js";
+import { State } from "react-powerplug";
 
 
 function App() {
 
+
   return (
-    <div className="App">
-        <div class="heading">
+      <>
+      <FirebaseAuthProvider {...config} firebase={firebase}>
+          <div>
+              <IfFirebaseUnAuthed>
 
-        </div>
-        <div class="tables">
-            <ul className="nav nav-tabs" id="myTab" role="tablist">
-                <li className="nav-item">
-                    <a className="nav-link active" id="pending-tab" data-toggle="tab" href="#pending" role="tab"
-                       aria-controls="pending" aria-selected="true">Pending</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" id="sent-tab" data-toggle="tab" href="#sent" role="tab"
-                       aria-controls="sent" aria-selected="false">sent</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" id="history-tab" data-toggle="tab" href="#history" role="tab"
-                       aria-controls="history" aria-selected="false">History</a>
-                </li>
-            </ul>
+                  <UnAuthedPage/>
 
-            <div className="tab-content" id="myTabContent">
-                <div className="tab-pane fade in active" id="pending" role="tabpanel" aria-labelledby="pending-tab">
-                    <OrdersContainer state={"pending"} title={"New Orders"}/>
-                </div>
-                <div className="tab-pane fade" id="sent" role="tabpanel" aria-labelledby="sent-tab">
-                    <OrdersContainer state={"sent"} title={"Sent Orders"}/>
-                </div>
-                <div className="tab-pane fade" id="history" role="tabpanel" aria-labelledby="history-tab">
-                    <OrdersContainer state={"all"} title={"All Orders"}/>
+              </IfFirebaseUnAuthed>
+              <IfFirebaseAuthed>{() => <AuthedPage/>}</IfFirebaseAuthed>
+          </div>
+      </FirebaseAuthProvider>
+
+          </>
+  );
+}
+class AuthedPage extends React.Component{
+    constructor(props) {
+        super(props);
+        console.log("constructor")
+    }
+render() {
+
+
+    return (
+        <div className="App">
+            <div className="heading">
+                <button onClick={async () => {
+                    await firebase
+                        .app()
+                        .auth()
+                        .signOut();
+                }}>
+                    Sign Out
+                </button>
+            </div>
+            <div className="tables">
+                <ul className="nav nav-tabs" id="myTab" role="tablist">
+                    <li className="nav-item">
+                        <a className="nav-link active" id="pending-tab" data-toggle="tab" href="#pending" role="tab"
+                           aria-controls="pending" aria-selected="true">Pending</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" id="sent-tab" data-toggle="tab" href="#sent" role="tab"
+                           aria-controls="sent" aria-selected="false">sent</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" id="history-tab" data-toggle="tab" href="#history" role="tab"
+                           aria-controls="history" aria-selected="false">History</a>
+                    </li>
+                </ul>
+
+                <div className="tab-content" id="myTabContent">
+                    <div className="tab-pane fade in active" id="pending" role="tabpanel" aria-labelledby="pending-tab">
+                        <OrdersContainer state={"pending"} title={"New Orders"}/>
+                    </div>
+                    <div className="tab-pane fade" id="sent" role="tabpanel" aria-labelledby="sent-tab">
+                        <OrdersContainer state={"sent"} title={"Sent Orders"}/>
+                    </div>
+                    <div className="tab-pane fade" id="history" role="tabpanel" aria-labelledby="history-tab">
+                        <OrdersContainer state={"all"} title={"All Orders"}/>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
+}
+
+
+class UnAuthedPage extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {username: '', password: '', signMode: true};
+
+
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handlerPasswordChange = this.handlerPasswordChange.bind(this);
+        this.handlerSignMode = this.handlerSignMode.bind(this);
+
+    }
+
+    handleUsernameChange(event) {
+        this.setState({username: event.target.value});
+    }
+
+    handlerPasswordChange(event){
+        this.setState({password: event.target.value});
+    }
+
+    handlerSignMode(){
+        console.log("hello?")
+        var active = this.state.signMode;
+        var newActive = active === true ? false : true;
+        this.setState({
+            signMode: newActive
+        });
+        console.log(this.state.signMode)
+    }
+
+    render (){
+        return(
+            <State initial={{ isLoading: false, error: null }}>
+                {({ state, setState }) => (
+                    <div>
+                        <div>isLoading : {JSON.stringify(state.isLoading)}</div>
+                        <div>error : {JSON.stringify(state.error)}</div>
+
+                        <div className="login-page">
+                            <div className="form">
+
+                                {this.state.signMode === false ? (
+                                    <form className="register-form">
+                                        <input type="text" placeholder="name"/>
+                                        <input type="text" onChange={this.handleUsernameChange}  value={this.state.username} placeholder="email address"/>
+                                        <input type="password" onChange={this.handlerPasswordChange} value={this.state.password} placeholder="password"/>
+                                        <button type={"button"} onClick={async () => {
+                                            setState({ isLoading: true, error: null });
+                                            await firebase
+                                                .app()
+                                                .auth()
+                                                .createUserWithEmailAndPassword(this.state.username, this.state.password);
+                                            setState({ isLoading: false, error: null });
+                                        }}>create</button>
+                                        <p className="message">Already registered? <a onClick={this.handlerSignMode}>Sign In</a></p>
+                                    </form>
+                                ) : (
+                                    <form className="login-form">
+                                        <input type="text" onChange={this.handleUsernameChange} value={this.state.username} placeholder="E-mail"/>
+                                        <input type="password" onChange={this.handlerPasswordChange} value={this.state.password} placeholder="Password"/>
+                                        <button type={"button"} onClick={async () => {
+                                            try{
+                                                setState({isLoading: true, error: null});
+                                                await firebase
+                                                    .app()
+                                                    .auth()
+                                                    .signInWithEmailAndPassword(this.state.username, this.state.password);
+                                                //etState({ isLoading: false, error: null, username: '' , password: ''});
+                                            }catch (error) {
+                                                setState({isLoading: false, error: error});
+                                            }
+                                        }}>login</button>
+                                        <div className="google-btn" onClick={async () => {
+                                            //try {
+                                                setState({ isLoading: true, error: null });
+                                                const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+                                                await firebase.auth().signInWithPopup(googleAuthProvider);
+                                                 setState({ isLoading: false, error: null });
+                                            /*} catch (error) {
+                                                setState({ isLoading: false, error: error });
+                                            }*/
+                                        }}>
+                                            <img className="google-icon"
+                                                 src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"/>
+
+                                        </div>
+                                        <p className="message">Not registered? <a onClick={this.handlerSignMode}>Create an account</a></p>
+                                    </form>
+                                )}
+
+                            </div>
+                        </div>
+
+                        <button
+
+                            onClick={async () => {
+                                setState({ isLoading: true, error: null });
+                                await firebase
+                                    .app()
+                                    .auth()
+                                    .signInWithEmailAndPassword("test@test.test", "testtest");
+                                setState({ isLoading: false, error: null });
+                            }}
+                        >
+                            Login with email and pass
+                        </button>
+                        <button
+
+                            onClick={async () => {
+                                try {
+                                    setState({ isLoading: true, error: null });
+                                    const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+                                    await firebase.auth().signInWithPopup(googleAuthProvider);
+                                    // setState({ isLoading: false, error: null });
+                                } catch (error) {
+                                    setState({ isLoading: false, error: error });
+                                }
+                            }}
+                        >
+                            Login With Google
+                        </button>
+                        <button
+
+                            onClick={async () => {
+                                setState({ isLoading: true, error: null });
+                                await firebase
+                                    .app()
+                                    .auth()
+                                    .createUserWithEmailAndPassword("test2@test.test", "testtest");
+                                setState({ isLoading: false, error: null });
+                            }}
+                        >
+                            register
+                        </button>
+                    </div>
+                )}
+            </State>
+        )
+    }
+};
 
 
 export default App;
---
+++ src/test-credentials.js
@@ -0,0 +1,9 @@
+export const config = {
+  apiKey: "AIzaSyAQdOGJ2FgQVCb850LfGI3DgXU6IEaaak4",
+  authDomain: "bot-test-f7307.firebaseapp.com",
+  databaseURL: "https://bot-test-f7307-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "bot-test-f7307",
+  storageBucket: "bot-test-f7307.appspot.com",
+  messagingSenderId: "454445711952",
+  appId: "1:454445711952:web:075f6a2d1724b6b4ad8755"
+};
